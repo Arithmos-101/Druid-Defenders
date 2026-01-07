2e8ce27c0ea2dcfb5aec2c19c3d06d52ca5d9574
Arithmos-101 <arithmos101@gmail.com>
2026-01-07 15:29:23 -0600

    player changes

--- player/player.gd
+++ player/player.gd
@@ -1,4 +1,5 @@
-extends Control
+extends Node
+class_name Player
 
 @export_group("Connections")
 @export var card_hand : CardHand
@@ -9,7 +10,14 @@ extends Control
 @export var starting_hand_size : int
 @export var deck : Array[int] # Array of card IDs
 
+var _starting_health := 10
+var _health : int
+
+func _init_stats() -> void:
+	_health = _starting_health
+
 func _ready() -> void:
+	_init_stats()
 	card_draw.connect("card_drawn", card_hand.append_card)
 	card_hand.connect("card_used", card_discard.append_card)
 	
@@ -18,4 +26,3 @@ func _ready() -> void:
 	card_deck.shuffle()
 	for card in card_deck.card_stack:
 		card_draw.append_card(card)
-		
